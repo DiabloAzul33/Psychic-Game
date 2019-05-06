4b3603165ce8e433baeaf132ce4ee9d5824dadde
DiabloAzul <curr33@gmail.com>
2019-05-05 21:47:08 -0600

    alerts working but counter/displays not

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
-// all potential computer guesses 
-var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+// all potential valid computer and user guesses 
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // wins, losses, guesses left and guesses made
 var wins = 0;
@@ -14,20 +14,23 @@ var lossesText = document.getElementById("losses");
 var userGuessText = document.getElementById("userGuess");
 var guessesRemainingText = document.getElementById("guessesRemaining");
 var guessesMadeText = document.getElementById("guessesMade");
-
-document.onkeyup = function (event) {
-    // storing the userGuess, lowercased for ease & consistency
-    var userGuess = event.key.toLowerCase();
     // generating a random computerGuess each round
-    var computerGuess = letters[Math.floor(Math.random() * letters.length)];
+var computerGuess = letters[Math.floor(Math.random() * letters.length)];
 
     function repeat() {
         guessesRemaining = 9;
         guessesMade = [];
         computerGuess = letters[Math.floor(Math.random() * letters.length)];
     }
+document.onkeyup = function (event) {
+    // storing the userGuess, lowercased for ease & consistency
+    var userGuess = event.key.toLowerCase();
+    console.log(userGuess);
+    console.log(computerGuess);
 
     // check to see whether the user won or lost
+if (letters.indexOf(userGuess) > -1){
+    if (guessesMade.indexOf(userGuess) === -1){
     if (userGuess === computerGuess) {
         wins++;
         repeat();
@@ -38,15 +41,16 @@ document.onkeyup = function (event) {
         losses++;
         repeat();
     }
-
     // after each turn update the HTML
     winsText.textContent = wins;
     lossesText.textContent = losses;
-    userGuessText.textContent = userGuess;
+    guessesMadeText.textContent = guessesMade;
     guessesRemainingText.textContent = guessesRemaining;
-
+    } else {
+        alert("Try a new letter to keep playing!")
+    }
     } else {
     // the user will get a message if they press any key other than a letter
     alert("Invalid guess! Press any LETTER to play!");
     }
-}   
+}
